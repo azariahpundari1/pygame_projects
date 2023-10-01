8c00228536b34dc5de57f6f1a3dd9eaee05f7376
azariahpundari1 <azariah.pundari@my.jcu.edu.au>
2023-10-02 00:28:57 +1000

    Add main screen

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import pygame
 
-#TODO implement basic window for viewing
+def main():
+    pygame.init()
+
+    pygame.display.set_caption("Bounce animation")
+
+    screen = pygame.display.set_mode((720, 360))
+    
+    # Game loop until quit
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
 #TODO Create game object -> rectangle (30, 30)
+
+
+if __name__ == "__main__":
+    main()
