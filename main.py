8733c7e9d4d7e9d37eff4077d2042c03b9e9251c
azariahpundari1 <azariah.pundari@my.jcu.edu.au>
2023-10-02 15:06:44 +1000

    Chnge rect location,chnge bg clr

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
     pygame.display.set_caption("Bounce animation")
-    player = pygame.Rect(0, 0, WIDTH, HEIGHT)
+    player = pygame.Rect(200, 200, WIDTH, HEIGHT)
 
     # Game loop until quit
     while True:
@@ -17,8 +17,8 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
-        screen.fill((255, 255, 255))
-        pygame.draw.rect(screen, (0, 0, 255), player)
+        screen.fill((0, 0, 0))
+        pygame.draw.rect(screen, (200, 200, 255), player)
         pygame.display.flip()  # updates screen
 
 
